88cf4cc08e5adc0c705b9802cee985b3d2658625
rosskata <rossdimitrov@gmail.com>
2023-05-20 22:10:38 +0000

    Format update

--- get_yahoo_data.py
+++ get_yahoo_data.py
@@ -1,37 +1,36 @@
-#iteratively get all ticker data for S&P500 companies
-#import libs
+# iteratively get all ticker data for S&P500 companies
+# import libs
 import datetime
-import csv
 import pandas_datareader as pdr
 
 startDate = "2012-01-01"
 today = datetime.date.today()
-#startDate= datetime.date.today()
+# startDate= datetime.date.today()
 
-infilename ="snp500_initial.csv"
-infile = open(infilename,"r")
+infilename = "snp500_list.csv"
+
+infile =  open(infilename, "r")
 
 while True:
-    ticker=infile.readline()
+    ticker = infile.readline()
 
     if not ticker:
-        break;
+      break
 
     try:
-      data = pdr.get_data_yahoo(ticker.strip(),startDate,today)
-      outfileName = ticker.strip()
-      outfileExt = ".csv"
-      outfileNameString = outfileName + outfileExt
-      outfile = open(outfileNameString,"w")
-      outfile.write(ticker)
-      outfile.write(data.to_string(index=False))
-      outfile.write("\n")
-      outfile.close()
-      print("---> Got ", ticker.strip()," data!")
+        data = pdr.get_data_yahoo(ticker.strip(), startDate, today)
+        outfileName = ticker.strip()
+        outfileNameString = outfileName + ".csv"
+        outfile = open(outfileNameString, "w")
+        outfile.write(ticker)
+        outfile.write(data.to_string(index=False))
+        outfile.write("\n")
+        outfile.close()
+        print("---> Got ", ticker.strip(), " data!")
 
     except ValueError:
-      print('ValueErrror')
+        print('ValueErrror')
 
 
-print("---=== GOT ALL TICKER DATA from ",startDate," to ",today," ===---")
+print("---=== GOT ALL TICKER DATA from ", startDate, " to ", today, " ===---")
 infile.close()
